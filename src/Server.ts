4f3d4aed47a1acb49ad5d25773834da00067c6ad
Abhishek Yadav <avhishekydv@gmail.com>
2020-05-09 22:59:51 +0530

    cors

--- src/Server.ts
+++ src/Server.ts
@@ -27,6 +27,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+app.use((req, res, next)=> {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
